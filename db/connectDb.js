31571ec0e8e784d0d1b8d2d7e8f4e8d1e8bfadf7
PIYALI <piyaofficial05@gmail.com>
2026-05-29 16:59:24 +0530

    Add database connection utility with mongoose

---
+++ db/connectDb.js
@@ -0,0 +1,18 @@
+// db.js
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://localhost:27017", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("MongoDB Connected Successfully");
+  } catch (error) {
+    console.error("MongoDB Connection Failed:", error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
